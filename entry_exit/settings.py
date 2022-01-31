306fb3ead2852fa60f82bbd2fc7552437073c631
abhaynit <87191075+abhaynit@users.noreply.github.com>
2022-01-31 18:56:08 +0530

    gjj

--- entry_exit/settings.py
+++ entry_exit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,7 +54,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'entry_exit.urls'
 
-import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'testapp/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
